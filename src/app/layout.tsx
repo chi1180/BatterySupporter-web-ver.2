171f3cc997567e766c03155df2bd15cf005cd2b7
chi1180 <cwchi1@outlook.jp>
2025-01-12 11:21:45 +0900

    meta tag style changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,14 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ReactNode } from "react";
-import Head from "next/head";
 
 export const metadata: Metadata = {
-  title: "BatterySupporter web",
-  description: "BatterySupporter web",
+  title: "BatterySupporter",
+  description:
+    "A useful desktop application for supporting your device's battery usage.",
+  verification: {
+    google: "dVRTS_9lJ9SL6_YPh2Il8afiUi0v_3EbbHesLSBdDeo",
+  },
 };
 
 export default function RootLayout({
@@ -17,17 +20,6 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <Head>
-          <title>BatterySupporter</title>
-          <meta
-            name="description"
-            content="A useful desktop application for supporting your device's battery usage."
-          />
-          <meta
-            name="google-site-verification"
-            content="dVRTS_9lJ9SL6_YPh2Il8afiUi0v_3EbbHesLSBdDeo"
-          />
-        </Head>
         <body>{children}</body>
       </html>
     </ClerkProvider>
