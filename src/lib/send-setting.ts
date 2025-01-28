204a55f7fbe3ef7cd4ad3bc5ecd0a8992d4d775a
chi1180 <cwchi1@outlook.jp>
2025-01-28 21:38:24 +0900

    safely type applied

--- src/lib/send-setting.ts
+++ src/lib/send-setting.ts
@@ -1,12 +1,21 @@
 import { settingPropertyNames } from "@/interfaces";
 import { getProperty } from "./setting-data";
 
+interface ElectronWindow extends Window {
+  electron?: {
+    sendSetting(): void;
+    isElectron: boolean;
+  };
+}
+
 export default function sendSetting(prop: settingPropertyNames) {
   if (typeof Window === "undefined") return;
   getProperty(prop);
-  if ("electron" in window) {
-    if ("sendSetting" in window?.electron) {
-      window?.electron?.sendSetting();
-    }
+
+  if (
+    "electron" in window &&
+    "sendSetting" in (window as ElectronWindow).electron!
+  ) {
+    (window as ElectronWindow).electron?.sendSetting();
   }
 }
